dd77c81bf14b73e360e6c3fa8100d61c18cfddc7
vnorr <vnorrgard@gmail.com>
2021-01-22 09:46:26 +0100

    remove margins and shrink height

--- get-started/src/index.tsx
+++ get-started/src/index.tsx
@@ -12,7 +12,7 @@ const useOnScreen = (ref: any) => {
 
       // setIntersecting(entry.isIntersecting);
     },
-    { rootMargin: "0px 0px -40% 0px" }
+    { rootMargin: "0px 0px -20% 0px" }
   );
 
   useEffect(() => {
@@ -110,25 +110,15 @@ const App = () => {
 
   return (
     <div>
-      <div style={{ height: "500px" }}></div>
+      {/* <div style={{ height: "500px" }}></div> */}
       <div className="step-container">
         {steps.map((data) => {
           return <Step key={data.id} {...data} />;
         })}
       </div>
-      <div style={{ height: "500px" }}></div>
+      {/* <div style={{ height: "500px" }}></div> */}
     </div>
   );
 };
-
-// 1. Contact us to get an account for your company - We are currently offering our data products for the corporate market.
-
-// 2. Sign in and register your application - The application you register will be a virtual representation of your real application.
-
-// 3. Get access to our data products - Agree to our terms and conditions and describe how your application will use our data.
-
-// 4. Start development – You will first get access to test environment. When you are happy with the testing, we will do a quick review to ensure that it is ready to go to production.
-
-// 5. Go Live! - Your digital solutions can now be enriched with our insurance data!
 
 ReactDOM.render(<App />, document.querySelector("#root"));
